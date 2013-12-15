9e5e62c6f4dce93d9d3e7f982f5ed33e22e672a6
Franz See <franz.see@gmail.com>
2013-12-15 16:09:06 +0800

    Created ArtifactProfileUtil.

--- src/main/java/see/fa/artifactpatcher/ArtifactPatcher.java
+++ src/main/java/see/fa/artifactpatcher/ArtifactPatcher.java
@@ -1,19 +1,14 @@
 package see.fa.artifactpatcher;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.NameFileFilter;
 import see.fa.artifactpatcher.models.*;
 import see.fa.artifactpatcher.util.FileUtil;
-import see.fa.artifactpatcher.util.XMLUtil;
 
-import java.io.*;
-import java.util.*;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.io.File;
+import java.util.Collection;
 
-import static see.fa.artifactpatcher.util.ChecksumUtil.shasum;
-import static see.fa.artifactpatcher.util.XMLUtil.readXML;
+import static see.fa.artifactpatcher.util.ArtifactProfileUtil.*;
 import static see.fa.artifactpatcher.util.XMLUtil.writeXML;
 import static see.fa.artifactpatcher.util.ZipUtil.unzip;
 import static see.fa.artifactpatcher.util.ZipUtil.zip;
@@ -22,13 +17,13 @@ public class ArtifactPatcher {
 
     public void execute(CreateArtifactProfile args) {
         File file = new File(args.getFile());
-        ArtifactProfile artifactProfile = toArtifactProfiles(file);
+        ArtifactProfile artifactProfile = createArtifactProfile(file);
         writeXML(artifactProfile, new File(args.getOutput()));
     }
 
     public void execute(CreatePatch args) {
         ArtifactProfile sourceArtifactProfile = readArtifactProfile(new File(args.getProfile()));
-        ArtifactProfile destinationArtifactProfile = toArtifactProfiles(new File(args.getFile()));
+        ArtifactProfile destinationArtifactProfile = createArtifactProfile(new File(args.getFile()));
 
         @SuppressWarnings("unchecked") Collection<FileArtifactProfile> newFiles = CollectionUtils.subtract(destinationArtifactProfile.getFiles(), sourceArtifactProfile.getFiles());
 
@@ -46,41 +41,6 @@ public class ArtifactPatcher {
 
     public void execute(ApplyPatch args) {
 
-    }
-
-    private List<String> toFileNames(Collection<FileArtifactProfile> fileArtifactProfiles) {
-        List<String> fileNames = new LinkedList<String>();
-        for (FileArtifactProfile fileArtifactProfile : fileArtifactProfiles) {
-            fileNames.add(fileArtifactProfile.getName());
-        }
-        return fileNames;
-    }
-
-    private ArtifactProfile readArtifactProfile(File file) {
-        ArtifactProfile artifactProfile = new ArtifactProfile();
-        readXML(file, artifactProfile);
-        return artifactProfile;
-    }
-
-    private ArtifactProfile toArtifactProfiles(File file) {
-        ZipInputStream zipInputStream = null;
-        try {
-            zipInputStream = new ZipInputStream(new FileInputStream(file));
-
-            ArtifactProfile artifactProfile = new ArtifactProfile();
-
-            ZipEntry zipEntry = zipInputStream.getNextEntry();
-            while(zipEntry != null) {
-                artifactProfile.getFiles().add(new FileArtifactProfile(zipEntry.getName(), shasum(zipInputStream)));
-
-                zipEntry = zipInputStream.getNextEntry();
-            }
-            return artifactProfile;
-        } catch (IOException e) {
-            throw new ArtifactPatcherException(String.format("Unable to read JAR/EAR/WAR//ZIP from dir '%s'.", new File(".").getAbsolutePath()), e);
-        } finally {
-            IOUtils.closeQuietly(zipInputStream);
-        }
     }
 
 }
---
+++ src/main/java/see/fa/artifactpatcher/util/ArtifactProfileUtil.java
@@ -0,0 +1,56 @@
+package see.fa.artifactpatcher.util;
+
+import org.apache.commons.io.IOUtils;
+import see.fa.artifactpatcher.ArtifactPatcherException;
+import see.fa.artifactpatcher.models.ArtifactProfile;
+import see.fa.artifactpatcher.models.FileArtifactProfile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static see.fa.artifactpatcher.util.ChecksumUtil.shasum;
+import static see.fa.artifactpatcher.util.XMLUtil.readXML;
+
+public class ArtifactProfileUtil {
+    
+    public static List<String> toFileNames(Collection<FileArtifactProfile> fileArtifactProfiles) {
+        List<String> fileNames = new LinkedList<String>();
+        for (FileArtifactProfile fileArtifactProfile : fileArtifactProfiles) {
+            fileNames.add(fileArtifactProfile.getName());
+        }
+        return fileNames;
+    }
+
+    public static ArtifactProfile readArtifactProfile(File file) {
+        ArtifactProfile artifactProfile = new ArtifactProfile();
+        readXML(file, artifactProfile);
+        return artifactProfile;
+    }
+
+    public static ArtifactProfile createArtifactProfile(File file) {
+        ZipInputStream zipInputStream = null;
+        try {
+            zipInputStream = new ZipInputStream(new FileInputStream(file));
+
+            ArtifactProfile artifactProfile = new ArtifactProfile();
+
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+            while(zipEntry != null) {
+                artifactProfile.getFiles().add(new FileArtifactProfile(zipEntry.getName(), shasum(zipInputStream)));
+
+                zipEntry = zipInputStream.getNextEntry();
+            }
+            return artifactProfile;
+        } catch (IOException e) {
+            throw new ArtifactPatcherException(String.format("Unable to read JAR/EAR/WAR//ZIP from dir '%s'.", new File(".").getAbsolutePath()), e);
+        } finally {
+            IOUtils.closeQuietly(zipInputStream);
+        }
+    }
+}
